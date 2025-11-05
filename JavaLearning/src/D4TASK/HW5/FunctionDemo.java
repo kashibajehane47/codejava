955976c2f7ba5af26c333a3e99df820a0bf73163
kashibajehane47 <kashibajehane47@gmail.com>
2025-11-05 11:31:24 +0530

    feat: added HW

---
+++ JavaLearning/src/D4TASK/HW5/FunctionDemo.java
@@ -0,0 +1,19 @@
+package D4TASK.HW5;
+import java.util.function.Function;
+
+public class FunctionDemo {
+    public static void main(String[] args) {
+
+        // Function to return length of a string
+        Function<String, Integer> stringLength = s -> s.length();
+        System.out.println("Length of string: " + stringLength.apply("Hello World!"));
+
+        // Function to convert String to uppercase
+        Function<String, String> toUpper = s -> s.toUpperCase();
+        System.out.println("Uppercase: " + toUpper.apply("kashiba jehane"));
+
+        // Function to square a number
+        Function<Integer, Integer> square = n -> n * n;
+        System.out.println("Square of 6: " + square.apply(6));
+    }
+}
